0bc40ad7202fb2726334089341ceb83ef9efaddf
yersans <yersans@hotmail.com>
2018-12-29 15:55:38 +0800

    Add pad method to zlju.

---
+++ ZL-JsUtils.js
@@ -0,0 +1,13 @@
+(function (global, factory) {
+    factory(global);
+}(window, function (global) {
+    var zlju = {};
+
+    zlju.pad = function (input, width, char) {
+        char = char || "0";
+        input = input + "";
+        return input.length >= width ? input : new Array(width - input.length + 1).join(char) + input;
+    };
+
+    global.zlju = zlju;
+}));
